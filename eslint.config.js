fc7df91e7bab1b1b7f15e1cf4b5eb3883b0083f2
Janos Jarecsni <janos.jarecsni@gmail.com>
2024-10-20 15:08:15 +0100

    :wrench: Add initial ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
 const js = require('@eslint/js');
 const globals = require('globals');
+const jestPlugin = require('eslint-plugin-jest');
 
 module.exports = [
     js.configs.recommended,
     {
+        files: ["**/*.test.js", "**/*.spec.js"],
         languageOptions: {
             ecmaVersion: 2022,
             sourceType: 'module',
             globals: {
                 ...globals.browser,
                 ...globals.node,
+                it: "readonly",
+                expect: "readonly",
+                describe: "readonly",
             }
-        }
+        },
+        plugins: {
+            jest: jestPlugin
+        },
     }
 ];

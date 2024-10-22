a9260cb80b85e8a50eeba96b1e36bb0344c6c6bd
Janos Jarecsni <johnny.jarecsni@icloud.com>
2024-10-22 06:46:07 +0100

    :wrench: Add Jest plugin (downgrade to version in the book)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
+/** @type {import('eslint').Linter.Config } */
 module.exports = {
     extends: ['eslint:recommended'],
     env: {
         es2021: true,
         node: true,
-    }
+    },
+    overrides: [
+        {
+            files: ['*.test.js'],
+            plugins: ['jest'],
+            extends: ['plugin:jest/recommended'],
+        },
+    ]
 }

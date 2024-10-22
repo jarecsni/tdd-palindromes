71815d53d3cf00ffee7ba64e0035c4c406710c74
Janos Jarecsni <johnny.jarecsni@icloud.com>
2024-10-22 07:34:51 +0100

    :sparkles: Simple palindrome implementation

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,20 @@
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
 module.exports = (str) => {
-  return [str];
+  const words = str.split(/\s+/); // Split the string into words
+  const palindromes = new Set();
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i; j < words.length; j++) {
+      const substring = words.slice(i, j + 1).join(' ');
+      if (isPalindrome(substring) && substring.length > 1) {
+        palindromes.add(substring);
+      }
+    }
+  }
+
+  return Array.from(palindromes);
 };
--- palindromes.test.js
+++ palindromes.test.js
@@ -1,8 +1,18 @@
-const palindromes = require("./palindromes");
+const palindromes = require('./palindromes');
 
-describe("palindromes", () => {
-  it("should identify one word palindromes", () => {
-    expect(palindromes("madam")).toEqual(["madam"]);
-    expect(palindromes("racecar")).toEqual(["racecar"]);
+describe('palindromes', () => {
+  it('should identify one word palindromes', () => {
+    expect(palindromes('madam')).toEqual(['madam']);
+    expect(palindromes('racecar')).toEqual(['racecar']);
+  });
+  it('should return an empty array when no palindromes are found', () => {
+    expect(palindromes('tic tac toe')).toEqual([]);
+  });
+  it('detects multi word palindromes', () => {
+    expect(palindromes('nurses run')).toEqual(['nurses run']);
+    expect(palindromes('nurses run fast')).toEqual(['nurses run']);
+    expect(palindromes('A man, a plan, a canal, Panama')).toEqual([
+      'A man, a plan, a canal, Panama',
+    ]);
   });
 });

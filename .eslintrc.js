d86ecc99638e83f07cec8f3d62144f7702eb33f9
Janos Jarecsni <johnny.jarecsni@icloud.com>
2024-10-22 07:03:07 +0100

    :wrench: Complete prettier setup, prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 /** @type {import('eslint').Linter.Config } */
 module.exports = {
-    extends: ['eslint:recommended', 'prettier'],
-    env: {
-        es2021: true,
-        node: true,
+  extends: ["eslint:recommended", "prettier"],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["*.test.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
     },
-    overrides: [
-        {
-            files: ['*.test.js'],
-            plugins: ['jest'],
-            extends: ['plugin:jest/recommended'],
-        },
-    ]
-}
+  ],
+};
